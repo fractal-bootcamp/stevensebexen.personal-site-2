889bacd115060886231e6a76809fbd03fc917109
Steven <ssebexen@gmail.com>
2024-08-08 15:56:15 -0400

    Added home page

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,28 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin='true'>
+<link href="https://fonts.googleapis.com/css2?family=Teko&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
+
 <script lang="ts">
     import { AppView } from "./types";
-
+    import HomeView
+     from "./views/home/HomeView.svelte";
   let view: AppView = AppView.Home;
+  $: currentView = {
+    [AppView.Home]: HomeView
+  }[view];
+
 </script>
 
 <main>
-  <h1>My site</h1>
+  <svelte:component this={currentView} />
 </main>
 
 <style>
   main {
+    display: flex;
     align-self: stretch;
+    align-items: stretch;
     flex: 1 1 0px;
   }
+
 </style>
---
+++ src/views/home/HomeView.svelte
@@ -0,0 +1,48 @@
+<script lang='ts'>
+    import Svg from '../../assets/svg/Title.svelte';
+</script>
+
+<div id='view-home'>
+  <Svg />
+  <p id='name'>Steven Sebexen</p>
+  <div id='next'>This is the point at which words fail to convey the meaning to which they were assigned.</div>
+</div>
+
+<style>
+  #view-home {
+    display: flex;
+    position: relative;
+    align-items: stretch;
+    flex: 1 1 0px;
+  }
+
+  #name {
+    cursor: default;
+    position: absolute;
+    left: 17%;
+    top: 35%;
+    transform: rotate(-12deg);
+    font-size: 8rem;
+    letter-spacing: 12px;
+    color: black;
+    font-family: "Teko";
+    animation: sway 6s ease-in-out 0s infinite alternate;
+  }
+
+  #next {
+    cursor: default;
+    position: absolute;
+    color: white;
+    font-family: "Space Grotesk";
+    font-size: 2rem;
+    width: 30%;
+    right: 6%;
+    bottom: 3%;
+    pointer-events: none;
+  }
+
+  @keyframes sway {
+    from {transform: rotate(-14deg);}
+    to {transform: rotate(-11deg);}
+  }
+</style>
