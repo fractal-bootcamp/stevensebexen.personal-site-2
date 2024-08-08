52a1aade82efd59fdabc81151f2d7ec89f184ef6
Steven <ssebexen@gmail.com>
2024-08-08 14:40:22 -0400

    Init

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { AppView } from "./types";
+
+  let view: AppView = AppView.Home;
+</script>
+
+<main>
+  <h1>My site</h1>
+</main>
+
+<style>
+  main {
+    align-self: stretch;
+    flex: 1 1 0px;
+  }
+</style>
