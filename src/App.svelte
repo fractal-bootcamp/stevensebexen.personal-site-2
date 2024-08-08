e73007e954dd2d00a44327c965c1e0239f604844
ssebexen <167637495+ssebexen@users.noreply.github.com>
2024-08-08 16:02:57 -0400

    Update src/App.svelte
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin='true'>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin='anonymous'>
 <link href="https://fonts.googleapis.com/css2?family=Teko&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
 
 <script lang="ts">
